f9f788ac06e86edf3ce77d37c6f11ed1c2038365
Darcy <github@darcythomas.com>
2016-09-17 11:24:34 +1200

    Renaming

---
+++ WebSite/IsolationHost/IsolationProxy.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsolationHost
+{
+    public class IsolationProxy: IDisposable
+    {
+        private readonly AppDomain _appDomain;
+        
+        public IsolationProxy()
+        {
+            _appDomain = AppDomain.CreateDomain("New Domain");
+
+            string file = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);// + "\\" + Parts[0].Trim() + ".dll";
+
+
+            Process.Start("explorer.exe", file);
+
+
+            //   _appDomain.ExecuteAssembly(@"c:\HelloWorld.exe");
+
+
+            //  appDomain.DoCallBack(new CrossAppDomainDelegate(SayHello));
+
+        }
+
+
+        public void Dispose()
+        {
+            //Close Appdomain
+            AppDomain.Unload(_appDomain);
+        }
+    }
+}
---
+++ WebSite/SecurityModule/SecurityModule.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SecurityModule
+{
+    public class SecurityModule
+    {
+        public bool HasAccess(String userName, String action)
+        {
+            return true;//We pretend to call the DB here and check if the user has permission to perform an action
+        }
+    }
+}
