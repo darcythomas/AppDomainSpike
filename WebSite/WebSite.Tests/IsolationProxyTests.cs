85f889627cac132c3c3bfe6eb174c1a72c96c641
Darcy <github@darcythomas.com>
2016-09-17 08:25:04 +1200

    stab at tests

---
+++ WebSite/WebSite.Tests/IsolationProxyTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IsolationWrapperStarter;
+using NUnit.Framework;
+
+namespace WebSite.Tests
+{
+    [TestFixture]
+   public class IsolationProxyTests
+    {
+        [Test]
+        public void StartupTest()
+        {
+             IsolationWrapper.StartUp puller;
+            using (IsolationProxy isolation = new IsolationProxy())
+            {
+
+                Console.WriteLine("here");
+
+
+            }
+            
+        }
+    }
+}
