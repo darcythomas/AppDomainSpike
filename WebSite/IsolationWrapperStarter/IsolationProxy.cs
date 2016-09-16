62e3879a62775ac3edc332df9e0b23a7be2b913a
Darcy <github@darcythomas.com>
2016-09-17 08:24:52 +1200

    Added appdomain starter project

---
+++ WebSite/IsolationWrapperStarter/IsolationProxy.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsolationWrapperStarter
+{
+    public class IsolationProxy: IDisposable
+    {
+        private AppDomain appDomain;
+        
+        public IsolationProxy()
+        {
+            appDomain = AppDomain.CreateDomain("New Domain");
+
+            string File = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);// + "\\" + Parts[0].Trim() + ".dll";
+
+            appDomain.ExecuteAssembly(@"c:\HelloWorld.exe");
+
+
+            //  appDomain.DoCallBack(new CrossAppDomainDelegate(SayHello));
+
+        }
+
+
+        public void Dispose()
+        {
+            //Close Appdomain
+            AppDomain.Unload(appDomain);
+        }
+    }
+}
